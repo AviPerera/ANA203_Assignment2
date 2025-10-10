4b382195ebae7d0eb8dda7052276a2340e2438ae
AviPerera <av.9@live.com>
2025-10-11 01:15:42 +1100

    CSV file loaded

--- Task1_OOP.py
+++ Task1_OOP.py
@@ -1,2 +1,14 @@
 #Task 1: Object Oriented Programming
 #Load CSV File
+
+import pandas as pd
+
+csv_file_path = "Sample - Superstore.csv"
+
+df = pd.read_csv(csv_file_path)
+
+#Print dataset shape
+print("Dataset rows and columns", df.shape)
+print(df.head())
+print(df.tail())
+print(df.describe())
