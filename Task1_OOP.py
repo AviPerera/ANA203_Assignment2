c9242afefd78f5f2c43a6bbe1d1391ef4ca9ecd8
AviPerera <av.9@live.com>
2025-10-12 20:47:46 +1100

    Encoding error fixed

--- Task1_OOP.py
+++ Task1_OOP.py
@@ -1,11 +1,12 @@
 #Task 1: Object Oriented Programming
 #Load CSV File
 
+
 import pandas as pd
 
 csv_file_path = "Sample - Superstore.csv"
 
-df = pd.read_csv(csv_file_path)
+df = pd.read_csv(csv_file_path, on_bad_lines='skip',encoding= 'latin-1')
 
 #Print dataset shape
 print("Dataset rows and columns", df.shape)
